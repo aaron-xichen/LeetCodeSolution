4c18712bc24626df82ffebec472e5645295b3819
aaron-xichen <aaron.xichen@gmail.com>
2015-04-26 12:09:04 +0800

    modify

--- 12/main.cpp
+++ 12/main.cpp
@@ -1,15 +1,9 @@
-#include<iostream>
-using namespace std;
-struct Node{
-    char val;
-    Node * pre;
-    Node * next;
-};
-class Solution{
-    public:
-        string intToRoman(int num){
-
-        }
-
-
-};
+#include "./solution2.cpp"
+int main(){
+    int ratings[] = {2,3,4,1};
+    int len = sizeof(ratings)/sizeof(ratings[0]);
+    vector<int> ratings_vec(ratings, ratings+len);
+    Solution sl = Solution();
+    int candy_num = sl.noDuplicated(ratings_vec, 0, len-1);
+    cout<<candy_num<<endl;
+}
