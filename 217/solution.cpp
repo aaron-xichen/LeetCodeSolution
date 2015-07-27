f6187f9b07ed8f091405dff9f106ec520771e238
aaron-xichen <aaron.xichen@gmail.com>
2015-07-28 07:32:24 +0800

    contains duplicate I

---
+++ 217/solution.cpp
@@ -0,0 +1,13 @@
+#include "../solution.h"
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for(int i=0; i<(int)nums.size(); i++){
+            if(s.find(nums[i]) != s.end())
+                return true;
+            s.insert(nums[i]);
+        }
+        return false;
+    }
+};
